95f93f1e8f3a923cfe65303ef235a7c76eb5c687
OgurcovM <112924148+OgurcovM@users.noreply.github.com>
2022-09-26 09:20:07 +0300

    Add files via upload

---
+++ lesson1/InstrumEEEnt/src/InstrumentTest.java
@@ -0,0 +1,13 @@
+public class InstrumentTest
+{
+    public static void main(String[] args) {
+        Instrument[] instruments = new Instrument[3];
+        instruments[0] = new Guitar();
+        instruments[1] = new Caisse();
+        instruments[2] = new Trumpet();
+
+        for (Instrument instrument : instruments) {
+            instrument.play();
+        }
+    }
+}
---
+++ lesson1/Printable/src/Magazine.java
@@ -0,0 +1,6 @@
+public class Magazine implements Printable{
+    @Override
+    public void print() {
+        System.out.println("Печать журнала");
+    }
+}
