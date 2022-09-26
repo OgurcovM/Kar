668c743146c57a67cf3f3fc79aca9ccf07309d19
OgurcovM <112924148+OgurcovM@users.noreply.github.com>
2022-09-26 09:18:48 +0300

    Delete Printable directory

--- Printable/src/Book.java
+++
@@ -1,6 +0,0 @@
-public class Book implements Printable {
-    @Override
-    public void print() {
-        System.out.println("Печать книги");
-    }
-}
--- Printable/src/Magazine.java
+++
@@ -1,6 +0,0 @@
-public class Magazine implements Printable{
-    @Override
-    public void print() {
-        System.out.println("Печать журнала");
-    }
-}
